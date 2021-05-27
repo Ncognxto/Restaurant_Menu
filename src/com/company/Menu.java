ad726fec8b5c5214302cecf0afd28e2dba5a32a7
Ncognxto <c.hicks2929@gmail.com>
2021-05-27 09:07:20 -0500

    Finished the Restaurant menu studio

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,34 +1,55 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Menu {
-    // Array lists to hold items
-    private ArrayList<MenuItem> appetizers = new ArrayList<>();
-    private ArrayList<MenuItem> main = new ArrayList<>();
-    private ArrayList<MenuItem> dessert = new ArrayList<>();
 
-    //constructors
-    public Menu(){
-        populateMenu();
+    private ArrayList<MenuItem> menuItems;
+    Scanner input;
+    int index;
+    MenuItem displayDish;
+
+    public Menu() {
+        this.menuItems = new ArrayList<>();
     }
 
-    //setters and getters
-
-
-    //methods
-    public void populateMenu (){
-        //appetizers
-        appetizers.add(new MenuItem("Bacon Jammin' Wings", 10.59, "Bone-In or Boneless wings topped with savory-sweet bacon jam, bacon crumbles, cherry peppers and BBQ brown sugar glaze. " +
-                "Served on a bed of Yukon kettle chips.", "Appetizers", false));
-        appetizers.add(new MenuItem("Fried Pickle Nickles", 6.29, "Golden-fried dill slices served with Campfire Mayo.", "Appetizers", false));
-        //main dishes
-        main.add(new MenuItem("Ensenada Chicken Platter", 14.89, "One or two ancho-grilled chicken breasts, house-made salsa and salsa-ranch dressing. " +
-                "Served with choice of side.", "Main Dish", false));
-        main.add(new MenuItem("Pub Mac & Cheese", 9.59, "Pasta, creamy cheese sauce, toasted crouton crumbs and Parmesan. " +
-                "Served with choice of side.", "Main Dish", false));
-        //desserts
-        dessert.add(new MenuItem("Mountain High Mudd Pie", 8.49, "Chocolate and vanilla ice cream, layered with OREO® cookies, fudge, caramel and whipped cream.", "Dessert", false));
-        dessert.add(new MenuItem("Gooey Chocolate Brownie Cake", 6.99, "Chocolate brownie cake with hot fudge and strawberry puree, served à la mode.", "Dessert", false));
+    //Add menu items to the menu:
+    public void addMenuItem(MenuItem menuItem) {
+        this.menuItems.add(menuItem);
     }
+
+    //Remove items from the menu:
+    public void removeMenuItem(MenuItem menuItem){
+        this.menuItems.remove(menuItem);
+    }
+
+    //Print out entire menu:
+    public void displayMenu(){
+        for(MenuItem item : menuItems){
+            item.output();
+        }
+    }
+    //Print one item from the menu
+    public void displayItem(){
+        input = new Scanner(System.in);
+        System.out.println("Please enter the number of the dish you would like to eat: ");
+        index = input.nextInt();
+        input.close();
+        displayDish = menuItems.get(index);
+
+        System.out.println("You have selected " + displayDish.getDishName() + ". The " +
+                displayDish.getDishName() + " is " + displayDish.getDescription() + " for only $"
+                + displayDish.getPrice() + ".");
+    }
+
+    void getDate(){
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+        Date date = new Date();
+        System.out.println(sdf.format(date));
+
+    }
+
+
+
 }
--- src/com/company/MenuItem.java
+++ src/com/company/MenuItem.java
@@ -1,20 +1,135 @@
 package com.company;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class MenuItem {
 
-    private String name;
-    private double price;
-    private String description;
     private String category;
-    private boolean isNew = true;
+    private String dishName;
+    private String description;
+    private double price;
+    private String newDish;
 
-    public MenuItem (String name, double price, String description, String category, boolean isNew) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
+    Scanner scanner = new Scanner(System.in);
+
+    public MenuItem(String category, String dishName, String description, double price, String newDish) {
         this.category = category;
-        this.isNew = isNew;
+        this.dishName = dishName;
+        this.description = description;
+        this.price = price;
+        this.newDish = newDish;
+    }
+    public MenuItem(String category, String dishName, String description, double price) {
+        this.category = category;
+        this.dishName = dishName;
+        this.description = description;
+        this.price = price;
+        this.newDish = "a classic";
+    }
+    public MenuItem(){
+
     }
 
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String aCategory) {
+        category = aCategory;
+    }
 
+    public String getDishName() {
+        return dishName;
+    }
+    public void setDishName(String aDishName) {
+        dishName = aDishName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String aDescription){
+        description = aDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double aPrice){
+        price = aPrice;
+    }
+
+    public String getNewDish() {
+        return newDish;
+    }
+    public void setNewDish(String aNewDish) {
+        newDish = aNewDish;
+    }
+
+    public void output(){
+        System.out.println(this.dishName + ":\n\t" + this.description + " is  a " + this.category
+                + " for $" + this.price + ".\n\tThis item is " + this.newDish + ".");
+    }
+
+    //Ask user to input a new Menu item
+    public MenuItem createNewItem(){
+        String input;
+        //keeping the do-while statement to allow user to create multiple items to add to the menu.
+        //Just nee do figure out how to do so.
+        do{
+            System.out.println("If you would like to create a new menu item enter a number 1-4:");
+            System.out.println("Enter 1 to create a new appetizer item:");
+            System.out.println("Enter 2 to create a new entre item:");
+            System.out.println("Enter 3 to create a new dessert item: ");
+            System.out.println("Enter 4 to exit");
+
+            String aCategory = "";
+            input = scanner.nextLine();
+            if (input.equals("1")){
+                aCategory = "appetizer";
+            }else if (input.equals("2")){
+                aCategory = "entre" ;
+            }else if (input.equals("3")){
+                aCategory = "dessert";
+            }
+            setCategory(aCategory);
+
+            System.out.println("Please enter the name of your new dish: ");
+            String aDishName = scanner.nextLine();
+            setDishName(aDishName);
+
+            System.out.println("Please enter a description for the new dish: ");
+            String aDescription = scanner.nextLine();
+            setDescription(aDescription);
+
+            System.out.println("Please enter the price for the new dish: ");
+            double aPrice = scanner.nextDouble();
+            setPrice(aPrice);
+
+            String aNewDish = "new";
+            setNewDish(aNewDish);
+
+            return new MenuItem(category, dishName, description, price, newDish);
+        }while (!input.equals("4"));
+
+    }
+
+    @Override
+    public boolean equals(Object dish) {
+        if (this == dish)
+            return true;
+        if (!(dish instanceof MenuItem))
+            return false;
+        MenuItem menuItem = (MenuItem) dish;
+        return Objects.equals(getDishName(), menuItem.getDishName()) && Objects.equals(getDescription(), menuItem.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDishName(), getDescription());
+    }
+//
+//    public void duplicateItem(){
+//
+//    }
 }
